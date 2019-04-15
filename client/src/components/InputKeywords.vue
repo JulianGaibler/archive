18f157b0a7034f1946d2a37f6f42e991b1d0fea4
JulianWels <julian@jwels.berlin>
2019-04-15 02:23:29 +0200

    :construction: Further work on client

--- client/src/components/InputKeywords.vue
+++ client/src/components/InputKeywords.vue
@@ -1,36 +1,49 @@
 <template>
     <div class="inputField light">
-        <input :placeholder="label" @input="handleInput" />
+        <div class="autocomplete">
+            <div v-for="item in content" :key="item" class="tag"><span>{{valueStore[item]}}</span>X</div>
+            <input :placeholder="label" v-model="searchWord" @input="handleInput" />
+
+
+            <ul class="results">
+                <li v-for="keyword in keywords" :key="keyword.id" @click="addItem(keyword)" class="result">{{keyword.name}}</li>
+            </ul>
+        </div>
         <hr>
         <label :class="{ visible: showLabel }">{{label}}</label>
     </div>
 </template>
 
 <script>
+import keywordSearch from '../graphql/keywordSearch.gql'
 
 export default {
-    name: 'InputField',
+    name: 'InputKeywords',
     props: {
         value: Array,
         label: String,
     },
     data() {
         return {
-            content: this.value
+            content: this.value,
+            valueStore: {},
+            searchWord: '',
         }
     },
-    //apollo: {
-    //    keywords: {
-    //        query: ME,
-    //        update(r) {
-    //            if (r) return r.me
-    //            else return null;
-    //        },
-    //        error(e) {
-    //            console.log('errors', e.message)
-    //        }
-    //    }
-    //},
+    apollo: {
+        keywords: {
+            query: keywordSearch,
+            variables () {
+                return {
+                    input: this.searchWord
+                }
+            },
+            debounce: 500,
+            error(e) {
+                console.log('errors', e.message)
+            }
+        }
+    },
     methods: {
         handleInput() {
             this.$emit('input', this.content)
@@ -40,10 +53,16 @@ export default {
             this.$refs.textarea.style.height = ''
             this.$refs.textarea.style.height = this.$refs.textarea.scrollHeight+'px'
         },
+        addItem(item) {
+            if (!!this.valueStore[item.id]) return;
+            this.valueStore[item.id] = item.name
+            this.content.push(item.id)
+            this.$emit('input', this.content)
+        },
     },
     computed: {
         showLabel() {
-            return this.content && this.content.length > 0
+            return (this.content && this.content.length) > 0 || (this.searchWord && this.searchWord.length > 0)
         }
     },
 }
