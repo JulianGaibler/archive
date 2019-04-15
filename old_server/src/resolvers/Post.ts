d5a6ee3d6a783d7aa12eba6ba674dccabfcdc5a7
JulianWels <julian@jwels.berlin>
2019-04-15 02:21:05 +0200

    :fire: Removed lots of migration files

--- old_server/src/resolvers/Post.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from '../utils'
-
-export const Post = {
-  uploader: ({ id }, args, ctx: Context) => {
-    return ctx.prisma.post({ id }).uploader()
-  },
-}
--- server-2/src/schemas/index.ts
+++
@@ -1,4 +0,0 @@
-import * as Author from './author';
-import * as Book from './book';
-
-export { Author, Book };
