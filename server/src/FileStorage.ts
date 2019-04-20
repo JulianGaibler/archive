0befea77443f427b34bf55d92a4d6088e97633dd
JulianWels <julian@jwels.berlin>
2019-04-20 03:57:02 +0200

    :sparkles: File Storage can now process videos, gifs and images correctly

--- server/src/FileStorage.ts
+++ server/src/FileStorage.ts
@@ -3,6 +3,7 @@ import jet from 'fs-jetpack'
 import sharp from 'sharp'
 import fileType from 'file-type'
 import ffmpeg from 'fluent-ffmpeg'
+import tmp from 'tmp'
 
 const options = {
     dist: 'public',
@@ -11,167 +12,163 @@ const options = {
     original: 'original',
 }
 
-export interface Upload {
-    filename: string,
-    mimetype: string,
-    encoding: string,
-    createReadStream: any,
-}
-export interface StorageInfo {
-    relHeight: number,
-    originalPath: string,
-    compressedPath: string,
-    thumbnailPath: string,
-}
 
-export async function checkFile(createReadStream) {
-    return fileType.stream(createReadStream());
-}
-
-export const storeFS2 = (stream, filename, transform?) => {
-    return new Promise((resolve, reject) => {
-        if (transform) return stream()
-            .pipe(transform)
-            .pipe(jet.createWriteStream(filename))
-            .on('error', error => reject(error))
-            .on('finish', () => resolve({ filename }))
-        else return stream()
-            .pipe(jet.createWriteStream(filename))
-            .on('error', error => reject(error))
-            .on('finish', () => resolve({ filename }))
-    })
-}
-
-export async function storeFile({createReadStream}: Upload) {
+export function storeFile(createReadStream) {
     return new Promise( async (resolve, reject) => {
 
-        const type = await checkFile(createReadStream)
-        if (type.fileType == null) throw Error('File Format not recognized')
-
+        const s = await fileType.stream(createReadStream());
+        const {ext, mime} = await s.fileType;
+        const kind = getKind(mime)
         const filename = uuid()
 
-        let video = isVideo(mimetype)
+        let createdFiles = []
 
-        let compressedPath
-        let thumbnailPath
+        if (kind === 'video') await processVideo(s, filename)
+        if (kind === 'image') await processImage(s, filename)
 
-        if (video) {
-            compressedPath = await createCompressedVideo(createReadStream, filename)
-        } else {
-            compressedPath = await createCompressedImage(createReadStream, filename)
-        }
-        if (video) {
-            thumbnailPath = await createThumbnailVideo(createReadStream, filename)
-        } else {
-            thumbnailPath = await createThumbnailImage(createReadStream, filename)
-        }
-
-        let originalPath = await storeOriginalFile(createReadStream, filename, type.fileType)
+        storeOriginal(s, filename, ext)
 
         resolve({
-            relHeight: 0,
-            compressedPath,
-            thumbnailPath,
-            originalPath,
+            compressedPath: `${options.compressed}/${filename}`,
+            thumbnailPath: `${options.thumbnail}/${filename}`,
+            originalPath: `${options.original}/${filename}.${ext}`,
         })
     })
 }
 
-function createCompressedImage(createReadStream, filename: string) {
-    //ffmpeg
+async function createThumbnail(readStream, filename: string) {
+
+    const compressed = jet.path(options.dist, options.thumbnail, filename)
+
+    const wsJpeg = jet.createWriteStream(`${compressed}.jpeg`)
+    const wsWebp = jet.createWriteStream(`${compressed}.webp`)
+
+    const transform = sharp().removeAlpha().resize(400, 400, {
+        fit: sharp.fit.inside,
+    })
+
+    transform.clone().toFormat('jpeg', { quality: 50, progressive: true }).pipe(wsJpeg)
+    transform.clone().toFormat('webp', { quality: 50 }).pipe(wsWebp)
+
+    readStream.pipe(transform)
 }
 
-async function storeOriginalFile(createReadStream, filename, {ext}) {
+async function processImage(readStream, filename: string) {
+
+    const compressed = jet.path(options.dist, options.compressed, filename)
+
+    const wsPng = jet.createWriteStream(`${compressed}.png`)
+    const wsWebp = jet.createWriteStream(`${compressed}.webp`)
+
+    const transform = sharp().removeAlpha().resize(900, 900, {
+        fit: sharp.fit.inside,
+        withoutEnlargement: true,
+    })
+
+    transform.clone().toFormat('png', { progressive: true }).pipe(wsPng)
+    transform.clone().toFormat('webp', { quality: 90, nearLossless: true }).pipe(wsWebp)
+
+    readStream.pipe(transform)
+
+    await createThumbnail(readStream, filename)
+}
+
+function storeFS(stream, filename) {
+  return new Promise((resolve, reject) => {
+        console.log('started')
+        stream
+            .on('error', error => {
+              if (stream.truncated)
+              reject(error)
+            })
+            .pipe(jet.createWriteStream(filename))
+            .on('error', error => {
+                console.log('error here', error)
+                reject(error)
+            })
+            .on('finish', () => {
+                console.log('done here')
+                resolve(filename)
+            })
+  })
+}
+
+async function processVideo(readStream, filename: string) {
+    const compressed = jet.path(options.dist, options.compressed, filename)
+
+    // Store video in our own temp-file
+    const tmpFile = tmp.fileSync()
+    await storeFS(readStream, tmpFile.name)
+
+    // Create temp dir for screenshot -_-
+    let tmpDir = tmp.dirSync();
+    let tmpFilename = `${filename}.png`;
+
+    console.log('start');
+
+    await (() => new Promise((resolve, reject) => {
+       ffmpeg(tmpFile.name)
+           .screenshots({
+               timestamps: [1],
+               filename: tmpFilename,
+               folder: tmpDir.name,
+           })
+           .on('error', e => {
+               console.log('ffmpeg ss:', e)
+               reject(e)
+           })
+           .on('end', ()=>{
+               resolve()
+           })
+    }))()
+
+    const tmpPath = jet.path(tmpDir.name, tmpFilename)
+
+    console.log(tmpPath)
+
+
+    const { height } = await sharp(tmpPath).metadata()
+
+    await createThumbnail(jet.createReadStream(tmpPath), filename)
+
+    jet.remove(tmpPath)
+    tmpDir.removeCallback()
+
+    const outputHeight = height > 720 ? 720 : height
+
+    // Video generator
+    ffmpeg(tmpFile.name)
+        .output(`${compressed}.mp4`)
+        .size(`?x${outputHeight}`)
+        .outputOptions(['-pix_fmt yuv420p', '-deinterlace', '-vsync 1', '-vcodec libx264', '-b:v: 2500k', '-bufsize 2M', '-maxrate 4500k', '-profile:v main', '-tune film', '-g 60', '-x264opts no-scenecut', '-acodec aac', '-b:a 192k', '-ac 2', '-ar 44100', '-f mp4'])
+        .run()
+
+    ffmpeg(tmpFile.name)
+        .output(`${compressed}.webm`)
+        .size(`?x${outputHeight}`)
+        .outputOptions(['-pix_fmt yuv420p', '-deinterlace', '-vsync 1', '-c:v libvpx-vp9', '-cpu-used 2', '-b:v: 2000k', '-bufsize 1000k', '-maxrate 3000k', '-c:a libopus', '-b:a 192k', '-f webm',])
+        .run()
+
+    // ToDo: Delete temp-files
+}
+
+async function storeOriginal(readStream, filename, ext) {
     const path = jet.path(options.dist, options.original, `${filename}.${ext}`)
-    await storeFS2(createReadStream, path)
+    const ws = jet.createWriteStream(path)
+    readStream.pipe(ws)
     return path
 }
 
-function createCompressedImage(createReadStream, filename: string) {
-    const basePipeline = () => sharp().removeAlpha().resize(900, 900, {
-        fit: sharp.fit.inside,
-        withoutEnlargement: true,
-    })
-    const exportAs = [{
-        format: 'png',
-        options: {
-            progressive: true,
-
-        },
-    },{
-        format: 'webp',
-        options: {
-            quality: 90,
-            nearLossless: true,
-        },
-    }]
-    let exportConfs = exportAs.map((conf) => {
-        return {
-            type: conf.format,
-            sharp: basePipeline().toFormat(conf.format, conf.options)
-        }
-    })
-
-    const dir = `${options.compressed}/${filename}`
-    return saveImage(createReadStream, dir, exportConfs)
-}
-
-function createThumbnailImage(createReadStream, filename: string) {
-    const basePipeline = () => sharp().removeAlpha().resize(400, 400, {
-        fit: sharp.fit.inside,
-        withoutEnlargement: true,
-    })
-    const exportAs = [{
-        format: 'jpeg',
-        options: {
-            quality: 50,
-            progressive: true,
-
-        },
-    },{
-        format: 'webp',
-        options: {
-            quality: 50,
-        },
-    }]
-    let exportConfs = exportAs.map((conf) => {
-        return {
-            type: conf.format,
-            sharp: basePipeline().toFormat(conf.format, conf.options)
-        }
-    })
-
-    const dir = `${options.thumbnail}/${filename}`
-    return saveImage(createReadStream, dir, exportConfs)
-}
-
-
-
-async function saveImage(createReadStream, dir, exportConfs) {
-    return new Promise(async resolve => {
-        for (var i = exportConfs.length - 1; i >= 0; i--) {
-            const filepath = `${dir}.${exportConfs[i].type}`
-            await storeFS2(
-                createReadStream,
-                jet.path(options.dist, filepath),
-                exportConfs[i].sharp
-            )
-        }
-        resolve(dir)
-    })
-}
-
-function isVideo(mimetype: String): boolean {
+function getKind(mimetype: String): string {
     let video;
+    // Treat gifs as videos
+    if (mimetype === 'image/gif') return 'video'
     switch (mimetype.split('/')[0]) {
         case 'image':
-            video = false
-            break;
+            return 'image'
         case 'video':
-            video = true
-            break;
+            return 'video'
         default:
             throw Error('Unsupported File-Type')
     }
-    return video
 }
