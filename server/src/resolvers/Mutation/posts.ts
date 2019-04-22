2e4b6a4e6f39de3ae2b517b0b85a9a284afcecc4
JulianWels <julian@jwels.berlin>
2019-04-22 02:57:03 +0200

    :sparkles: FileStorage queue now working (yay!)

--- server/src/resolvers/Mutation/posts.ts
+++ server/src/resolvers/Mutation/posts.ts
@@ -1,15 +1,18 @@
-import { storeFile } from '../../FileStorage'
-import fs from 'fs'
 import { getUserId, to } from '../../utils'
+import { UserInputError } from 'apollo-server'
+import graphqlFields from 'graphql-fields'
+
+import Task from '../../models/Task'
 
 export const posts = {
 
     async uploadPosts(parent, { items }, ctx, info) {
-
-        console.log(items)
         const userId = getUserId(ctx)
 
         if (!items || items.length < 1) throw new Error(`You have to at least upload one file.`)
+
+        let error = false
+        let results = []
 
         for (var i = items.length - 1; i >= 0; i--) {
             let fields = items[i];
@@ -18,14 +21,34 @@ export const posts = {
             if (!file) throw fileErr
             
             delete fields.file
+            fields.uploaderId = userId
 
-            let [storeDataErr, storeData] = await to(ctx.fileStorage.checkFile(fields, file.createReadStream()))
-            if (!storeData) throw storeDataErr
+            let resItem = await to(ctx.fileStorage.checkFile(fields, file.createReadStream()))
+            results.push(resItem)
+            if (!resItem[1]) error = true
         }
 
-        throw new Error(`YOU THINK YOU CAN TRICK ME? NEVER! Oh wait it's my fault [500]`)
+        if (error) {
+            const errors = results.map((item, idx) => {
+                return { index: idx, error: item[0] }
+            })
+            .filter(item => item.error)
+            console.log(errors)
+            throw new UserInputError('Upload Items invalid', errors);
+        }
 
-        return []
+        let taskIds = []
+
+        for (var i = items.length - 1; i >= 0; i--) {
+            const taskId = await ctx.fileStorage.storeFile(results[i][1])
+            taskIds.push(taskId)
+        }
+
+        const topLevelFields = Object.keys(graphqlFields(info));
+        let query = Task.query().findByIds(taskIds);
+        if (topLevelFields.includes('uploader')) query = query.eager('uploader')
+        if (topLevelFields.includes('createdPost')) query = query.eager('createdPost')
+        return await query
 
     },
 
