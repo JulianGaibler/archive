78b268abc4842a49532801f4e51f0638ed12a864
JulianWels <julian@jwels.berlin>
2019-06-10 16:51:12 +0200

    :heavy_plus_sign: Added localization package.

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,13 @@ import { createProvider } from './vue-apollo'
 
 import './assets/styles/main.styl'
 import router from './router'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
-  apolloProvider: createProvider(),
-  router,
-  render: h => h(App)
+    apolloProvider: createProvider(),
+    router,
+    i18n,
+    render: h => h(App)
 }).$mount('#app')
