a9ff3d82d3aaa65a79a769eef7c0586e4d777724
JulianWels <julian@jwels.berlin>
2019-06-14 23:55:53 +0200

    :sparkles: taskUpdates now can take list of Task IDs

--- server/src/schema/subscriptions/Task.ts
+++ server/src/schema/subscriptions/Task.ts
@@ -1,18 +1,35 @@
-import joinMonster from 'join-monster'
-import db from '../../database'
+import { withFilter } from 'graphql-subscriptions';
 import { decodeHashId, isAuthenticated, Context } from '../../utils'
 import {
     GraphQLFieldConfig,
+    GraphQLList,
+    GraphQLString,
     GraphQLNonNull,
 } from 'graphql'
 
 import { TaskUpdate } from '../types'
+import TaskModel from '../../models/Task'
 
 export const taskUpdates: GraphQLFieldConfig<any, any, any> = {
-    description: `Returns a list of tasks.`,
+    description: `Returns updates from tasks.`,
     type: new GraphQLNonNull(TaskUpdate),
+    args: {
+        ids: {
+            description: `List of Task IDs.`,
+            type: new GraphQLList(new GraphQLNonNull(GraphQLString))
+        }
+    },
     subscribe: (parent, args, context: Context, resolveInfo) => {
         isAuthenticated(context)
-        return context.pubSub.asyncIterator('taskUpdates')
+
+        const decodedIds = args.ids ? args.ids.map(id => +decodeHashId(TaskModel, id)) : false
+
+        return withFilter(
+            () => context.pubSub.asyncIterator('taskUpdates'),
+            (payload) => {
+                if (decodedIds && !decodedIds.includes(payload.taskUpdates.id)) return false
+                return true
+            },
+        )()
     }
 }
