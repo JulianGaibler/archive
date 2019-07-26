7806966375a6a9dfdb9929322f866b6526c3eea8
JulianWels <julian@jwels.berlin>
2019-07-26 16:52:11 +0200

    :sparkles: New shutdown scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,25 @@ import Server from './server'
 Database.connect()
 Server.start()
 
-const shutdown = done => {
-    Database.close(() => {
-        Server.stop()
-    })
-}
+process.on('exit', handleExit.bind(null, { cleanup: true }));
+process.on('SIGINT', handleExit.bind(null, { exit: true }));
+process.on('SIGTERM', handleExit.bind(null, { exit: true }));
+process.on('uncaughtException', handleExit.bind(null, { exit: true }));
 
-// Nodemon
-// process.on('exit', shutdown.bind(null, process.exit));
-// process.on('SIGINT', shutdown.bind(null, process.exit));
-// process.on('uncaughtException', shutdown.bind(null, process.exit));
+function handleExit(options, err) {
+    if (options.cleanup) {
+        const actions = [Server.stop, Database.close]
+        actions.forEach((close, i) => {
+            try {
+                close(() => {
+                    if (i === actions.length - 1) { process.exit() }
+                })
+            } catch (err) {
+                if (i === actions.length - 1) { process.exit() }
+            }
+        })
+    }
+    // tslint:disable-next-line:no-console
+    if (err) { console.error(err) }
+    if (options.exit) { process.exit() }
+}
