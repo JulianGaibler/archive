f1b53577190c8b30ea9a8fb33d7a94eb481e2c24
JulianWels <julian@jwels.berlin>
2019-07-31 22:22:20 +0200

    :bug: to-helper now converts undefined errors in generic ones to prevent false-negatives

--- server/src/utils/other.ts
+++ server/src/utils/other.ts
@@ -3,5 +3,5 @@ export function to(promise: Promise<any>) {
         .then(data => {
             return [null, data]
         })
-        .catch(err => [err])
+        .catch(err => [ err ? err : new Error('An error occurred') ]) // This ensures that an undefined error still evaluates to true
 }
