ddc82cb685af4f3bb779fd73e72ce89fb2e48b35
JulianWels <julian@jwels.berlin>
2019-08-29 22:24:19 +0200

    :sparkles: Added modal component

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="modalWrapper">
+        <div class="modal" v-click-outside="cancel">
+            <div class="message">
+                <p v-if="messageA"><strong>{{ messageA }}</strong></p>
+                <p v-if="messageB">{{ messageB }}</p>
+            </div>
+            <div class="actionsRow">
+                <button @click="cancel" class="button button-slim">{{ optionA }}</button>
+                <button @click="confirm" class="button button-slim" :class="[ important ? 'button-dangerous' : 'button-primary' ]">{{ optionB }}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Modal',
+    props: {
+        messageA: String,
+        messageB: String,
+        optionA: String,
+        optionB: String,
+        important: Boolean,
+    },
+    methods: {
+        cancel() {
+            this.$emit('cancel')
+        },
+        confirm() {
+            this.$emit('confirm')
+        },
+    },
+}
+</script>
